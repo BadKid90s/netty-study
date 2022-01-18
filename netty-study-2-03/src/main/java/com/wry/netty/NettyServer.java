c126e3fa062a9333313a2f805942c955326091bf
wangruiyu <wangruiyu@bhky.net.cn>
2022-01-18 14:54:04 +0800

    NettyServer收发数据

---
+++ netty-study-2-03/src/main/java/com/wry/netty/NettyServer.java
@@ -0,0 +1,51 @@
+package com.wry.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author 王瑞玉
+ * @since 2022/1/18 14:24
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+        new NettyServer().bing(9999);
+    }
+
+    private void bing(int port) {
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(boosGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast(new StringDecoder(CharsetUtil.UTF_8))
+                                    .addLast(new MyServerHandler())
+                            ;
+                        }
+                    });
+
+
+            ChannelFuture future = b.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
